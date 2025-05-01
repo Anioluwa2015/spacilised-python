fb804dc59adb1cd5ba5b904aab043c60fa97ba68
Anioluwa2015 <Anioluwa2015@gmail.com>
2025-05-01 18:23:02 +0100

    function asss yay

--- operators/caculator.py
+++ operators/caculator.py
@@ -1,13 +1,5 @@
-def add(a,b):
-    return a + b
-def minus(a,b):
-    return a - b
-def multiply(a,b):
-    return a * b
-def division(a,b):
-    return a / b
 
-print(add(6,6))
-print(minus(4,5))
-print(multiply(4,5))
-print(division(4,5))
+def multiply(a,b,c):
+    return a * b * c
+print(multiply(6,6,6))
+
