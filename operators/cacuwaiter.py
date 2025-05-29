789eb79990645437d88840e7719cc1aecd685bad
Anioluwa2015 <Anioluwa2015@gmail.com>
2025-05-29 16:16:20 +0100

    yes i have created a new project

---
+++ operators/cacuwaiter.py
@@ -0,0 +1,7 @@
+var=10
+while var>0:
+    var = var - 1
+    if var == 5:
+        continue
+    print("I want to make games and websites please" , var)
+print("please can I do website and games please!")
