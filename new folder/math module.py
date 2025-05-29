e2c7967927869e67217c59ba95e87deebd82b009
Anioluwa2015 <Anioluwa2015@gmail.com>
2025-05-29 18:07:28 +0100

    new modules!

---
+++ new folder/math module.py
@@ -0,0 +1,9 @@
+import math
+print(math.sqrt(66))
+print(math.ceil(22.5))
+print(math.floor(66.6))
+print(math.fabs(-6))
+print(math.factorial(6))
+print(math.copysign(-5,4))
+print(math.gcd(2,6))
+print(math.exp(66))
---
+++ new folder/ramdom module.py
@@ -0,0 +1,4 @@
+import random
+print(random.randint(60,166))
+print(random.random())
+print(random.choice("Ani,Shaza,Hamna,Emma"))
